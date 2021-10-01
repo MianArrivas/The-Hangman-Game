492a0e3c038d5f7542fea265089df881123f0558
Miguel Arriaga <mian.arrivas@hotmail.com>
2021-09-30 22:07:52 -0500

    Juego del ahorcado funcional

--- data_base_file.py
+++ data_base_file.py
@@ -4,8 +4,8 @@ from typing import List
 class Data_Base_File:
 	#region <Variables>
 	
-	__word_list : List
-	__number_list : List
+	__word_list = []
+	__number_list = []
 
 	#endregion
 
@@ -17,18 +17,21 @@ class Data_Base_File:
 				for count, word in enumerate( f ):
 					self.__number_list.append( count )
 					self.__word_list.append( word )
+		except Exception as ex:
+			print( ex )
 		except:
 			raise FileExistsError("No fue posible abrir la base de datos, verifique que exista el archivo data.txt")
 
 		return self.__number_list, self.__word_list		
 
 	def pick_random_word( self ) -> str:
+		self.get_word_list( )
 		return random.choice( self.__word_list )
 
 	def add_word( self, new_word: str ) -> None:
 		try:
 			with open( 'data.txt', 'a', encoding='utf-8' ) as f:
-				f.write( new_word )
+				f.write( f"\n{new_word.lower()}" )
 		except:
 			raise FileExistsError("Ha habido un error al agregar la palabra a la base de datos.")
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from data_base_file import Data_Base_File
 
@@ -7,18 +8,161 @@ from data_base_file import Data_Base_File
 DEC_VAL_ASCII_1 = 49
 DEC_VAL_ASCII_2 = 50
 
+PLAY = 1
+ADD_WORD = 2
+DELAY_CLEARING_TIME = 3
+
+NOT_ALLOWED_CHARACTERS = "¡!#$[]%&/()=¿?0123456789.:+-_*|°¬,;´¨\\<>^ \t" 
+
 #endregion
 
+#region <Variables>
+
 menu_selection : int
+attempt: int
+
+#endregion
 
 def print_title( ) -> None:
 	print( """
-		 ###   ### ######## #####    ###  #####    #####    ###### ######## #####    ###   #####
-		 ###   ### ###  ### ### ###  ### ###       ### ### ### ### ###  ### ### ###  ###  #######
-		 ######### ######## ###  ### ### #######   ###  #####  ### ######## ###  ### ###   #####
-		 ###   ### ###  ### ###   ###### ###  ###  ###         ### ###  ### ###   ######    ###
-		 ###   ### ###  ### ###    #####  ######   ###         ### ###  ### ###    #####     #
+		###   ### ######## #####    ###  #####    #####    ###### ######## #####    ###   #####
+		###   ### ###  ### ### ###  ### ###       ### ### ### ### ###  ### ### ###  ###  #######
+		######### ######## ###  ### ### #######   ###  #####  ### ######## ###  ### ###   #####
+		###   ### ###  ### ###   ###### ###  ###  ###         ### ###  ### ###   ######    ###
+		###   ### ###  ### ###    #####  ######   ###         ### ###  ### ###    #####     #
 	 """  )
+
+def draw_hangman( attempts: int ) -> None:
+	if attempts == 1:
+		print("""
+		   	#############
+		   	###         #
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		    ###
+		###########	
+		""")
+	if attempts == 2:
+		print("""
+		   	#############
+		   	###         #
+		   	###        ###
+		   	###       #   #
+		   	###        ###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		   	###
+		###########	
+		""")
+	if attempts == 3:
+		print("""
+		   	#############
+		   	###         #
+		   	###        ###
+		   	###       #   #
+		   	###        ###
+		   	###         #
+		   	###         #
+		   	###         #
+		   	###         #
+		   	###
+		   	###
+		   	###
+		###########	
+		""")
+	if attempts == 4:
+		print("""
+		   	#############
+		   	###         #
+		   	###        ###
+		   	###       #   #
+		   	###    #   ###
+		   	###     ##  #
+		   	###       ###
+		   	###         #
+		   	###         #
+		   	###
+		   	###
+		   	###
+		###########	
+		""")
+	if attempts == 5:
+		print("""
+		   	#############
+		   	###         #
+		   	###        ###
+		   	###       #   #
+		   	###    #   ###   #
+		   	###     ##  #  ##
+		   	###       #####
+		   	###         #
+		   	###         #
+		   	###
+		   	###
+		   	###
+		###########	
+		""")
+	if attempts == 6:
+		print("""
+		   	#############
+		   	###         #
+		   	###        ###
+		   	###       #   #
+		   	###    #   ###   #
+		   	###     ##  #  ##
+		   	###       #####
+		   	###         #
+		   	###         #
+		   	###        #
+		   	###       #
+		   	###
+		###########	
+		""")
+	if attempts == 7:
+		print("""
+		   	#############
+		   	###         #
+		   	###        ###
+		   	###       #   #
+		   	###    #   ###   #
+		   	###     ##  #  ##
+		   	###       #####
+		   	###         #
+		   	###         #
+		   	###        # #
+		   	###       #   #
+		   	###
+		###########	
+		""")
+
+def print_loser( ) -> None:
+	print("""
+	   	###       ######    ###### ######## ########    #####   #####   ##### 
+	   	###      ###  ### ###      ###      ###   ###  ####### ####### #######
+	   	###      ###  ###  ######  ######   #######     #####   #####   ##### 
+	   	###      ###  ###      ### ###      ###  ###     ###     ###     ###  
+	   	########  ######  #######  ######## ###   ###     #       #       #   
+	""")
+
+def print_winner( ) -> None:
+	print("""
+	   	###         ###  #######  #####    ###  #####    ###  ########  ########    #####   #####   ##### 
+	   	###         ###    ###    ### ###  ###  ### ###  ###  ###       ###   ###  ####### ####### #######
+	   	###  ####   ###    ###    ###  ### ###  ###  ### ###  ######    #######     #####   #####   ##### 
+	   	### ### ### ###    ###    ###   ######  ###   ######  ###       ###  ###     ###     ###     ###  
+	   	######   ######  #######  ###    #####  ###    #####  ########  ###   ###     #       #       #   
+	""")
 
 def clear_screen ( ) -> None:
 	os.system( "cls" )
@@ -39,9 +183,122 @@ def menu ( ) -> int:
 
 	return menu_selection
 
-def run( ) -> None:
+def add_word_to_db( new_word: str ) -> None:	
+	for char in new_word:
+		if char in NOT_ALLOWED_CHARACTERS:
+			raise ValueError(f"Se ha ingresado una palabra con caracteres no validos.\nLos siguientes caracteres no son válidos: \" {NOT_ALLOWED_CHARACTERS} \"")
+
+	db = Data_Base_File()
+	db.add_word(new_word)
+
+def uncover_underscore( underscore_string: str, position_to_uncover: int, guessed_letter: str) -> str:
+	# Convert the string into a List type so I can replace the the proper underscore
+	temp_str = list( underscore_string )
+
+	# Due to the underscore string I use uses the following format "_ _ _" (Underscore-space)
+	# I generate the right position to replace for a letter
+	position_to_uncover = 2 * position_to_uncover
+
+	temp_str[position_to_uncover] = guessed_letter
+
+	underscore_string = "".join(temp_str)
+
+	return underscore_string
+
+def game( ) -> None:
+	attempt = 0
+	used_letters = []
+
+	db = Data_Base_File( )
+	word_to_guess = db.pick_random_word( ).replace('\n', '')
+	word_size = len( word_to_guess )
+
+	letters_to_win = word_size
 	
-	menu( )
+	word_size_underscores = "_ "
+	for letter in range(word_size - 1):
+		word_size_underscores += "_ "
+
+	game_over = False
+	while not game_over:
+		#TODO: Crear el juego, pedir al usuario una letra, si la letra coincide con una de la palabra, agregar la letra a los dashes, de lo contrario, comienza a imprimir al ahorcado
+		clear_screen( )
+		print_title( )
+
+		print( f"\n\t\t> Letras faltantes para ganar: - {letters_to_win} - <\n" )
+
+		draw_hangman( attempt )
+
+		print( f"\n\t\t Palabra: {word_size_underscores}" )
+		
+		print(f"\n> Letras usadas: {used_letters} <")
+
+		if( attempt >= 7 ):
+			game_over = True
+			continue
+
+		if( letters_to_win <= 0):
+			break
+
+		letter = input( "\n\n\t Adivina una letra: " )
+
+		used_letters.append(f"{letter} - ")
+
+		letter_position = word_to_guess.find(letter)
+		if(letter_position != -1):
+			letters_to_win -= 1
+
+			word_size_underscores = uncover_underscore(word_size_underscores, letter_position, letter)
+			continue
+
+		attempt += 1
+	
+	print( f"\n\t\tLa palabra era: - {word_to_guess} - " )
+
+	if game_over:
+		print_loser()
+	else:
+		print_winner( )
+
+	time.sleep(3.5)
+
+	clear_screen( )
+	print_title( )
+
+def run( ) -> None:
+	clear_screen( )
+	
+	menu_selection = menu( )
+	
+	if menu_selection == PLAY:
+		game( )
+	
+	if menu_selection == ADD_WORD:
+		done = False
+
+		while not done:
+			word = input( "Introduzca una nueva palabra: " )
+			try:
+				add_word_to_db( word )
+
+				print( f"La palabra \'{word}\' ha sido añadida a la base de datos.")
+
+				done = True
+
+			except ValueError as ex:
+				print( ex )
+
+			except Exception as ex:
+				print( ex )
+
+			finally:
+				time.sleep( DELAY_CLEARING_TIME )
+
+				clear_screen( )
+				print_title( )
+
+
 
 if __name__ == '__main__':
-	run( )
+	while True:
+		run( )
